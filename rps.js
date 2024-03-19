0846acdf04ff338a025163dcf43eed8039fd76de
Alec Tiger Pinon <96355772+ATiger18@users.noreply.github.com>
2024-03-19 00:10:41 -0600

    Made rules for game

--- rps.js
+++ rps.js
@@ -1,1 +1,26 @@
 console.log("Hello World");
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+// Code that protects against case sensitivity goes here
+// Stuff here might just need to be HTML
+let choice = prompt('rock,paper, or scissors?');
+
+if (playerSelection == computerSelection) {
+    return ("Tie!");
+} else if (
+    (playerSelection === "rock" && computerSelection ==="scissors")||
+    (playerSelection === "scissors" && computerSelection === "paper")||
+    (playerSelection === "paper"&& computerSelection === "rock")
+) {
+    return "You won!" + playerSelection + "beats" + computerSelection;
+}
+
+
+
+
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
